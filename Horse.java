d8651bf81027f5a5bd54cd207e48cf9f114bcf28
Kharibin <ai.kharybin@gmail.com>
2017-05-09 05:03:12 +0300

    Horse upgrade
    added Fields:
    name speed distance
    added Setters, getters
    added constructor (name, speed, distance)

--- Horse.java
+++ Horse.java
@@ -4,4 +4,41 @@ package com.javarush.task.task21.task2113;
  * Created by Java on 09.05.2017.
  */
 public class Horse {
+
+    //fields
+    private String name;
+    private double speed, distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+
+    //Constructor
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
 }
