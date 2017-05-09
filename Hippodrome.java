bb1555fbf2dc98c8d814fdb0bfcafcbe78da11b7
Kharibin <ai.kharybin@gmail.com>
2017-05-09 04:57:51 +0300

    Constructor added
    Added constructor

--- Hippodrome.java
+++ Hippodrome.java
@@ -6,9 +6,18 @@ import java.util.List;
  * Created by Java on 09.05.2017.
  */
 public class Hippodrome {
+
+    //Fields and getters
     private List<Horse> horses;
+
     public List<Horse> getHorses(){
         return this.horses;
+    }
+
+
+    //Constructors
+    public Hippodrome(List<Horse> horses){
+        this.horses = horses;
     }
 
     public static void main(String[] args){
