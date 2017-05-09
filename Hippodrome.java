dd4e91e7b98415be8bb09c6412105eb860816aa8
Kharibin <ai.kharybin@gmail.com>
2017-05-09 05:14:54 +0300

    0.2b
    Initialising first hippodrome

--- Hippodrome.java
+++ Hippodrome.java
@@ -1,11 +1,13 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Java on 09.05.2017.
  */
 public class Hippodrome {
+    static Hippodrome game;
 
     //Fields and getters
     private List<Horse> horses;
@@ -22,6 +24,11 @@ public class Hippodrome {
 
     public static void main(String[] args){
 
+
+        game = new Hippodrome(new ArrayList<>());
+        for (int i = 0; i < 3; i++){
+            game.horses.add(new Horse("horse"+i, 3, 0));
+        }
 
 
     }
