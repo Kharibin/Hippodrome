fab49983b5540a1cb38f03c1b3f0c04a9ec1406e
Kharibin <ai.kharybin@gmail.com>
2017-05-09 04:53:17 +0300

    added Horses
    List<Horses> added to hippodrom and getter for it

--- Hippodrome.java
+++ Hippodrome.java
@@ -1,10 +1,19 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.List;
+
 /**
  * Created by Java on 09.05.2017.
  */
 public class Hippodrome {
+    private List<Horse> horses;
+    public List<Horse> getHorses(){
+        return this.horses;
+    }
+
     public static void main(String[] args){
+
+
 
     }
 
